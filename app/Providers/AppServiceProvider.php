fd6df18e5b544bb549462d24803c3888c10a1ee4
huanglei <huanglei@quanjingshuju.com>
2021-06-08 12:51:37 +0800

    feature: ide helper register when needed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         if ($this->app->environment('local')) {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
             $this->app->register(\Laravel\Tinker\TinkerServiceProvider::class);
             $this->app->register(\Laravel\Sail\SailServiceProvider::class);
