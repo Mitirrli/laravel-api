80b5333ae0a1618cc544ca7552807c8e6240ce4a
huanglei <huanglei@quanjingshuju.com>
2021-06-09 17:54:28 +0800

    feature: 获取oss签名

--- config/oss.php
+++ config/oss.php
@@ -4,5 +4,7 @@ return [
     'key' => env('OSS_ACCESS_KEY', ''),
     'secret' => env('OSS_ACCESS_SECRET', ''),
     'region' => env('OSS_REGION', 'cn-shanghai'),
+    'bucket' => env('OSS_BUCKET', ''),
+    'endpoint' => env('OSS_ENDPOINT', ''),
     'role' => env('OSS_ROLE_ARN', '')
 ];
