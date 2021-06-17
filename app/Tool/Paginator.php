b65acad529a13ecaa8bdd425b7f6d4635bdbfa47
huanglei <huanglei@quanjingshuju.com>
2021-06-17 16:53:33 +0800

    perf: 优化项目结构

---
+++ app/Tool/Paginator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tool;
+
+use Illuminate\Pagination\LengthAwarePaginator;
+
+
+class Paginator extends LengthAwarePaginator
+{
+  public function toArray()
+  {
+    return [
+      'data'  => $this->items->toArray(),
+      'page'  => $this->currentPage(),
+      'total_num' => $this->total(),
+    ];
+  }
+}
