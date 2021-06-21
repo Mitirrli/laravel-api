5971e3b3bf656b64c884e41029d5521dceeb0cbf
huanglei <huanglei@quanjingshuju.com>
2021-06-21 10:47:04 +0800

    perf: 优化opcache

--- config/database.php
+++ config/database.php
@@ -142,7 +142,7 @@ return [
         'client' => env('REDIS_CLIENT', 'phpredis'),
 
         'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'cluster' => env('REDIS_CLUSTER', 'phpredis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
