17e1d271c85b0066c4b13e88c9504251b40db00c
huanglei <huanglei@quanjingshuju.com>
2021-06-28 11:18:09 +0800

    test: 部署调试

--- build.sh
+++ build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-php artisan lighthouse:clear-cache
-php artisan lighthouse:cache
-php artisan config:cache
-php artisan route:cache
-php artisan event:cache
-php artisan view:cache
-
 supervisord --nodaemon --configuration /etc/supervisord.conf
